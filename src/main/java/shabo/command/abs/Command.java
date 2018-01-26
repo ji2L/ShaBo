c6420fa248369fc5920621f31dafda908003fd18
Maxime Samson <19492170+ji2L@users.noreply.github.com>
2018-01-26 20:28:42 +0100

    Better organize the command package

--- src/main/java/shabo/command/abs/Command.java
+++ src/main/java/shabo/command/abs/Command.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package shabo.command;
+package shabo.command.abs;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/shabo/command/abs/ICommand.java
+++ src/main/java/shabo/command/abs/ICommand.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package shabo.command;
+package shabo.command.abs;
 
 import javax.annotation.Nonnull;
 
