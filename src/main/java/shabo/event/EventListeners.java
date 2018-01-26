b1543d43b574997374bc61886ede4cffff9ad0c5
Maxime Samson <maxime.samson1@gmail.com>
2018-01-26 18:07:33 +0100

    Add EventListeners class

---
+++ src/main/java/shabo/event/EventListeners.java
@@ -0,0 +1,105 @@
+/*
+ * ShaBo - Discord BOT
+ * Copyright (C) 2018 - ji2L
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shabo.event;
+
+import java.util.Random;
+
+import net.dv8tion.jda.client.entities.Group;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.PrivateChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class EventListeners extends ListenerAdapter {
+	
+	public EventListeners() {
+	}
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        JDA jda = event.getJDA();
+        long responseNumber = event.getResponseNumber();
+
+        //Event specific information
+        User author = event.getAuthor();                //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //The MessageChannel that the message was sent to.
+
+        String msg = message.getContentDisplay();		//A human readable version of the Message.
+
+        boolean bot = author.isBot();
+
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
+        {
+        	//Guild specific information
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //The Member that sent the message.
+
+            String name;
+            if (message.isWebhookMessage())
+            {
+                name = author.getName();                //If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
+            else
+            {
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
+
+            System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
+        }
+        else if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        {
+            PrivateChannel privateChannel = event.getPrivateChannel();
+
+            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+        }
+        else if (event.isFromType(ChannelType.GROUP))   //If this message was sent to a Group. This is CLIENT only!
+        {
+            Group group = event.getGroup();
+            String groupName = group.getName() != null ? group.getName() : "";  //A group name can be null due to it being unnamed.
+
+            System.out.printf("[GRP: %s]<%s>: %s\n", groupName, author.getName(), msg);
+        }
+
+        if (msg.equals("!ping")) //Command : !ping/pong!
+        {
+            channel.sendMessage("pong!").queue();
+        }
+        else if (msg.equals("!roll")) //Command : !roll
+        {
+            Random rand = new Random();
+            int roll = rand.nextInt(6) + 1;
+            channel.sendMessage("Your roll: " + roll).queue(sentMessage ->
+            	{
+            		if (roll < 3)
+            			channel.sendMessage("The roll for messageId: " + sentMessage.getId() + " wasn't very good... Must be bad luck!\n").queue();
+            	}
+            );
+        }
+    }
+}
